d5f4c6ddec7d519ec7443f38c5c71685714a6714
misol2281 <estefanymichelle44@gmail.com>
2024-10-31 11:12:41 -0600

    Update CRUDEmpleado.java

--- SastreriaProyecto/Sastreria/src/java/Interfaces/CRUDEmpleado.java
+++ SastreriaProyecto/Sastreria/src/java/Interfaces/CRUDEmpleado.java
@@ -1,7 +1,13 @@
 
 package Interfaces;
 
+import java.util.List;
+import Entidad.Empleados;
 
 public interface CRUDEmpleado {
-    
+    public List listar();
+    public Empleados list(int id);
+    public boolean add(Empleados emp);
+    public boolean edit(Empleados emp);
+    public boolean eliminar(int id);
 }
