d557b7c0a3a59f9fdf4eeca998f750e47e9da0de
misol2281 <estefanymichelle44@gmail.com>
2024-10-31 12:07:29 -0600

    Create ClienteDAO.java

---
+++ SastreriaProyecto/Sastreria/src/java/Modelo/ClienteDAO.java
@@ -0,0 +1,64 @@
+
+package Modelo;
+
+import Config.Conexion;
+import Entidad.Cliente;
+import Interfaces.CRUDCliente;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class ClienteDAO implements CRUDCliente{
+
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    Cliente c = new Cliente();
+    
+    
+    @Override
+    public List listar() {
+        ArrayList<Cliente>list = new ArrayList<>();
+        String sql = "select * from Cliente";
+        try{
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Cliente cli = new Cliente();
+                cli.setId(rs.getInt("idCliente"));
+                cli.setNombre(rs.getString("Nombre"));
+                cli.setApellido(rs.getString("Apellido"));
+                cli.setTelefono(rs.getString("Telefono"));
+                list.add(cli);
+            }
+        }catch(Exception e){
+            
+        }
+        return list;
+    }
+
+    @Override
+    public Cliente list(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Cliente cli) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean edit(Cliente cli) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
