83947364b29b778f0ae030126c341116d6cfa718
misol2281 <estefanymichelle44@gmail.com>
2024-10-31 11:12:35 -0600

    Update CRUDCliente.java

--- SastreriaProyecto/Sastreria/src/java/Interfaces/CRUDCliente.java
+++ SastreriaProyecto/Sastreria/src/java/Interfaces/CRUDCliente.java
@@ -1,7 +1,13 @@
 
 package Interfaces;
 
+import java.util.List;
+import Entidad.Cliente;
 
 public interface CRUDCliente {
-    
+    public List listar();
+    public Cliente lis(int id);
+    public boolean add(Cliente cli);
+    public boolean edit(Cliente cli);
+    public boolean eliminar(int id);
 }
