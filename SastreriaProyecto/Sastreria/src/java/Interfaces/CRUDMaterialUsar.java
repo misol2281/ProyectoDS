2b9488d98393a9b839baa9681c578617a764e662
misol2281 <estefanymichelle44@gmail.com>
2024-10-31 12:07:27 -0600

    Update CRUDMaterialUsar.java

--- SastreriaProyecto/Sastreria/src/java/Interfaces/CRUDMaterialUsar.java
+++ SastreriaProyecto/Sastreria/src/java/Interfaces/CRUDMaterialUsar.java
@@ -1,7 +1,14 @@
 
 package Interfaces;
 
+import java.util.List;
+import Entidad.MaterialUsar;
 
 public interface CRUDMaterialUsar {
+    public List listar();
+    public MaterialUsar list(int id);
+    public boolean add(MaterialUsar mu);
+    public boolean edit(MaterialUsar mu);
+    public boolean eliminar(int id);
     
 }
