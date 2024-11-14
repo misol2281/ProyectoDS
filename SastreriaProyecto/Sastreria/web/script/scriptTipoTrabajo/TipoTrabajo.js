a6da9c0dba98ab063672e6f1a429eeee72680d0e
CristianAleRivas <161617671+CristianAleRivas@users.noreply.github.com>
2024-11-14 09:52:55 -0600

    Avance en agregar

--- SastreriaProyecto/Sastreria/web/script/scriptTipoTrabajo/TipoTrabajo.js
+++ SastreriaProyecto/Sastreria/web/script/scriptTipoTrabajo/TipoTrabajo.js
@@ -6,29 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
     // Agregar el evento de clic al botón
     abrir.addEventListener("click", function() {
         // Abrir otro JSP en una nueva ventana
-        window.open("vistas/vistaTipoTrabajo/agregarTipoTrabajo.jsp", "_blank", "width=800,height=600"); 
+        const pestanaAbrir = window.open("vistas/vistaTipoTrabajo/agregarTipoTrabajo.jsp", "_blank", "width=800,height=600"); 
+        pestanaAbrir.onload = function () {
+            
+        console.log("La nueva pestaña ha cargado");
         agregar.style.display = "block";
-        console.log("Hola");
-        // Puedes ajustar el tamaño de la ventana y otras opciones (como "scrollbars=yes" si necesitas barras de desplazamiento)
-    });
+        var agregar = document.getElementById("agregar");
+        var cerrarAgregar = document.getElementById("cerrarAgregar");
+        cerrarAgregar.onclick = function() {
+            agregar.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target === agregar) {
+                agregar.style.display = "none";
+            }
+        };
+        };
+                });
 });
-
-var agregar = document.getElementById("agregar");
-var abrirAgregar = document.getElementById("abrirAgregar");
-var cerrarAgregar = document.getElementById("cerrarAgregar");
-
-
-cerrarAgregar.onclick = function() {
-    agregar.style.display = "none";
-};
-
-
-window.onclick = function(event) {
-    if (event.target === agregar) {
-        agregar.style.display = "none";
-    };
-};
-
 function confirmarBorrar(){
     if(confirm("Desea eliminar este registro?")){
         console.log("Registro eliminado");
