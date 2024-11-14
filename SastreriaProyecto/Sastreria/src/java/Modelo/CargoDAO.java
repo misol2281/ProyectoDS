465e94430983a7a0a8ca768b46de200c2d95918e
misol2281 <estefanymichelle44@gmail.com>
2024-11-14 13:12:37 -0600

    Update CargoDAO.java

--- SastreriaProyecto/Sastreria/src/java/Modelo/CargoDAO.java
+++ SastreriaProyecto/Sastreria/src/java/Modelo/CargoDAO.java
@@ -21,9 +21,17 @@ public class CargoDAO implements CRUDCargo {
     @Override
     public List listar() {
         ArrayList<Cargo>list = new ArrayList<>();
-        String sql = "Slect * from Cargos";
+        String sql = "Select * from Cargos";
         try{
             con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Cargo car = new Cargo();
+                car.setId(rs.getInt("idCargo"));
+                car.setCargo(rs.getString("Cargo"));
+                list.add(car);
+            }
         }catch (Exception e){
             
         }
@@ -32,22 +40,58 @@ public class CargoDAO implements CRUDCargo {
 
     @Override
     public Cargo list(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "Select * from Cargo where idCargo="+id;
+        try{
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                c.setId(rs.getInt("idCargo"));
+                c.setCargo(rs.getString("Cargo"));
+            }
+        }catch(Exception e){
+            
+        }
+        return c;
     }
 
     @Override
     public boolean add(Cargo car) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "insert into Cargos(Cargo) values ('"+car.getCargo()+"')";
+        try{
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();
+        }catch(Exception e){
+            
+        }
+        return false;
     }
 
     @Override
     public boolean edit(Cargo car) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "update Cargos set Cargo = '"+car.getCargo()+"' where idCargo ="+car.getId();
+        try{
+           con = cn.getConnection();
+           ps = con.prepareStatement(sql);
+           ps.executeUpdate(); 
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
     public boolean eliinar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       String sql = "delete from Cargos where idCargo="+id;
+       try{
+           con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();
+       }catch(Exception e){
+           
+       }
+       return false;
     }
     
 }
