34417c701cacdba062979d603986af0117469238
misol2281 <estefanymichelle44@gmail.com>
2024-11-14 21:13:19 -0600

    Create Roles.java

---
+++ SastreriaProyecto/Sastreria/src/java/Entidad/Roles.java
@@ -0,0 +1,32 @@
+package Entidad;
+
+
+public class Roles {
+    int id;
+    String Rol;
+
+    public Roles() {
+    }
+
+    public Roles(String Rol) {
+        this.Rol = Rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return Rol;
+    }
+
+    public void setRol(String Rol) {
+        this.Rol = Rol;
+    }
+    
+    
+}
