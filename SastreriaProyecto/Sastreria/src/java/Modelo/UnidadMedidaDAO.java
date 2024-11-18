eb52e120141b079e253f37e1ab0967000477559f
misol2281 <estefanymichelle44@gmail.com>
2024-11-18 14:51:02 -0600

    Create UnidadMedidaDAO.java

---
+++ SastreriaProyecto/Sastreria/src/java/Modelo/UnidadMedidaDAO.java
@@ -0,0 +1,34 @@
+package Modelo;
+
+import Entidad.UnidadMedida;
+import Interfaces.CRUDUnidadMedida;
+import java.util.List;
+
+public class UnidadMedidaDAO implements CRUDUnidadMedida {
+
+    @Override
+    public List listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UnidadMedida list(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(UnidadMedida umd) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean edit(UnidadMedida umd) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
