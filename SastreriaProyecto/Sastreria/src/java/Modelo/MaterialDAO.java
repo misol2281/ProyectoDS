2c1a602cfd860228107e1ea048e7043c5d9e0dcf
misol2281 <estefanymichelle44@gmail.com>
2024-11-18 14:51:05 -0600

    Create MaterialDAO.java

---
+++ SastreriaProyecto/Sastreria/src/java/Modelo/MaterialDAO.java
@@ -0,0 +1,34 @@
+package Modelo;
+
+import Entidad.Material;
+import Interfaces.CRUDMaterial;
+import java.util.List;
+
+public class MaterialDAO implements CRUDMaterial {
+
+    @Override
+    public List listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Material list(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Material mat) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean edit(Material mat) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
